dda27d5476573b7116727b0b3d8f35c57c94cb01
susmitasanti <2021.susmita.santi@ves.ac.in>
2023-07-23 19:30:34 +0530

    changes in environment variables

--- backend/database.js
+++ backend/database.js
@@ -1,4 +1,10 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '.env.local') });
+
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASS:', process.env.DB_PASS);
+console.log('DB_NAME:', process.env.DB_NAME);
 
 const config = {
     db: {
